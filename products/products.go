038abb7f62fae4fc4ddbff5064a6c3ef967e1b04
braswelljr <braswellkenneth7@gmail.com>
2023-10-11 18:32:41 +0000

    fix: purge services

--- products/products.go
+++ products/products.go
@@ -1,1 +1,33 @@
 package products
+
+import (
+	"context"
+	"fmt"
+
+	"encore.app/pkg/middleware"
+	"encore.app/products/store"
+)
+
+// Get - Get a product
+//
+//	@param ctx - context.Context
+//	@param id
+//	@return product
+//	@return error
+//
+// encore:api auth method=GET path=/products/:id
+func Get(ctx context.Context, id string) (*store.Product, error) {
+	// check for claims
+	claims, err := middleware.GetVerifiedClaims(ctx, "")
+	if err != nil {
+		return &store.Product{}, err
+	}
+
+	// check for the roles
+	if !claims.HasRole(middleware.RoleSuperAdmin, middleware.RoleAdmin) {
+		return &store.Product{}, fmt.Errorf("unauthorized: you are not authorized to perform this action")
+	}
+
+	// return user
+	return &store.Product{}, nil
+}
